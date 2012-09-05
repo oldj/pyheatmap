467af89f315a97c929c6fc829d300f18f0509e48
oldj <oldj.wu@gmail.com>
2012-09-05 13:47:22 +0800

    添加setup.py。

---
+++ pyheatmap/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# author: oldj
+# blog: http://oldj.net
+# email: oldj.wu@gmail.com
+#
+
+__version__ = "0.1"
---
+++ pyheatmap/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pyheatmap",
+    version=__import__("__init__").__version__,
+    packages=[""],
+    url="https://github.com/oldj/pyheatmap",
+    license="LGPL",
+    author="oldj",
+    author_email="oldj.wu@gmail.com",
+    description="Create heatmap by Python."
+)
