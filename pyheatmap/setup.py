e53c150e186124bf4ce1f38cbc0c034533a92134
oldj <oldj.wu@gmail.com>
2012-09-05 14:31:55 +0800

    将setup.py移回子目录下。

--- pyheatmap/setup.py
+++ pyheatmap/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(
     name="pyheatmap",
-    version=__import__("pyheatmap.__init__").__version__,
-    packages=["pyheatmap"],
+    version=__import__("__init__").__version__,
+    packages=["inc"],
     url="https://github.com/oldj/pyheatmap",
     license="LGPL",
     author="oldj",
