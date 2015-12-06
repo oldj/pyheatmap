72e014552120673a423ebd643d961168363546c1
IChocolateKapa <echochocolatekapa@163.com>
2015-12-06 18:39:20 +0800

    用urllib2代替urllib

--- examples/test_python3.py
+++ examples/test_python3.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'kevin'
 
-import urllib.request
+import urllib2
 from pyheatmap.heatmap import HeatMap
 
 
@@ -10,8 +10,9 @@ def main():
     data = []
     # download test data
     url = "https://raw.github.com/oldj/pyheatmap/master/examples/test_data.txt"
-    for line in urllib.request.urlopen(url):
-        line_str = str(line, encoding='utf-8').split("\n")
+    for line in urllib2.urlopen(url):
+        #, encoding='utf-8'
+        line_str = str(line).split("\n")
         for ln in line_str:
             if ',' in ln:
                 a = ln.split(",")
